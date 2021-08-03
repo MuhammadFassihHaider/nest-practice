ae934fc5f0b44af006719725943769da596d7551
Muhammad Fassih Haider <fassih.haider786@gmail.com>
2021-08-03 18:03:23 +0500

    feat: :sparkles: connect with mongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,41 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CowsController } from './cows/cows.controller';
+import { CowsModule } from './cows/cows.module';
+import { CowsService } from './cows/cows.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CatsModule } from './cats/cats.module';
+const uri =
+  'mongodb+srv://fassih123:fassih123@devconnector.bcnhw.mongodb.net/devConnector';
+
+const connect = async () => {
+  try {
+    return MongooseModule.forRoot(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    //   TypeOrmModule.forRoot({
+    //   type: "mongodb",
+    //   database: "devConnector",
+    //   username: "fassih123",
+    //   password: "fassih123"
+    // }),
+    connect(),
+    CowsModule,
+    CatsModule,
+  ],
+  controllers: [AppController, CowsController],
+  providers: [AppService, CowsService],
 })
 export class AppModule {}
